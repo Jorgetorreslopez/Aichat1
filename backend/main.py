a0ba66bcd20cbb749fcc33df7c5151637d55fe06
Jorgetorreslopez <usda4ever@gmail.com>
2023-12-15 12:54:29 -0600

    Connected OpenAI/ElevenLabs and created text to speech functionality

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,66 @@
-from fastapi import FastAPI
+# uvicorn main:app 
+# uvicorn main:app --reload
 
+
+# Main Imports  --------------------------------
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from decouple import config
+import openai
+
+# Custom Function Imports
+from functions.openai_requests import convert_audio_to_text
+
+
+# Initiate App
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    print("Hello")
-    return {"message": "Hello World"}
+# CORS - Origins
+origins = [
+  "http://localhost:5173"  
+  "http://localhost:5174"  
+  "http://localhost:4173"  
+  "http://localhost:4174"  
+  "http://localhost:3000"  
+]
+
+
+# CORS - Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Check Health
+@app.get("/health")
+async def check_health():
+    return {"message": "healthy"}
+
+
+# Get audio
+@app.get("/post-audio-get/")
+async def get_audio():
+
+    # Get saved audio
+    audio_input = open("VoiceTest.mp3", "rb")
+
+    # Decode Audio
+    message_decoded = convert_audio_to_text(audio_input)
+
+    print(message_decoded)
+
+    return "DONE"
+
+
+# # Post bot response
+# # Note: Not playing in browser when using post request
+# @app.post("/post-audio/")
+# async def post_audio(file: UploadFile = File(...)):
+
+#     print("hello")
--- frontend/src/App.js
+++
@@ -1,6 +0,0 @@
-function App() {
-    return (<div className="">
-      <div className="text-2xl bg-blue-500">Hello</div>
-    </div>);
-}
-export default App;

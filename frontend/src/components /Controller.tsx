bed5bc04b4b1b648c11f5f899b0f207ad0219b2d
Jorgetorreslopez <usda4ever@gmail.com>
2023-12-28 12:48:21 -0600

    Finished Vivi Lang Prototype

--- frontend/src/components /Controller.tsx
+++ frontend/src/components /Controller.tsx
@@ -54,13 +54,62 @@ function Controller() {
           });
       });
 
-    setIsLoading(false);
+    // setIsLoading(false);
   };
 
   return (
     <div className="h-screen overflow-y-hidden">
       <Title setMessages={setMessages} />
       <div className="flex flex-col justify-between h-full overflow-y-scroll pb-96">
+        {/* conversation */}
+        <div className="mt-5 px-5">
+          {messages.map((audio, index) => {
+            return (
+              <div
+                key={index + audio.sender}
+                className={
+                  "flex flex-col " +
+                  (audio.sender == "Vivi" && "flex items-end")
+                }
+              >
+                {/* sender */}
+                <div className="mt-4">
+                  <p
+                    className={
+                      audio.sender == "Vivi"
+                        ? "text-right italic mr-2 text-pink-600"
+                        : "ml-2 italic text-purple-700"
+                    }
+                  >
+                    {audio.sender}
+                  </p>
+                  {/* Audio Message */}
+                  <audio
+                    src={audio.blobURL}
+                    className="appearance-none"
+                    controls
+                  />
+                </div>
+              </div>
+            );
+          })}
+
+          {messages.length == 0 && !isLoading && (
+            <div className="text-center font-light italic mt-10">
+              {" "}
+              Send Vivi A Message...
+            </div>
+          )}
+
+          {isLoading && (
+            <div className="text-center font-light italic mt-10 animate-pulse">
+              {" "}
+              Vivi Is Thinking...
+            </div>
+          )}
+        </div>
+
+        {/* Recorder */}
         <div className="fixed bottom-0 w-full py-6 border-t text-center bg-gradient-to-r from-purple-700 to-pink-600">
           <div className="flex justify-center items-center w-full">
             <RecordMessage handleStop={handleStop} />
